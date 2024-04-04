c8528c146f610f5ab1d9bd24da223d8ecde17bbb
Quentin Lecourt <quentin.lecourt@protonmail.com>
2024-04-04 09:27:21 -0400

    Ajout des boutons et des routes
    Routes edit et delete
    Boutons Modifier et Supprimer

--- resources/views/garderie.blade.php
+++ resources/views/garderie.blade.php
@@ -18,6 +18,18 @@
                 <td><h3> {{ $uneGarderie->ville }}</h3></td>
                 <td><h3> {{ $uneGarderie->province->description }}</h3></td>
                 <td><h3> {{ $uneGarderie->telephone }}</h3></td>
+                <td><form method="GET" action="/garderies/{{ $uneGarderie->id }}/edit">
+                    @csrf
+                    <button type="submit">Modifier</button>
+                </form>
+            </td>
+            <td>
+                <form method="POST" action="/garderies/{{ $uneGarderie->id }}/delete">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" onclick="return confirm('Voulez-vous vraiment supprimer cette garderie ?')">Supprimer</button>
+                </form>
+            </td>
                 
             </tr>
             @endforeach
